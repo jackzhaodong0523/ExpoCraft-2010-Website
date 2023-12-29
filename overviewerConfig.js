3f34dc3d0ed369a1424908bbfbf7f4fb7ec9525d
jackzhaodong0523 <jackzhaodong@qq.com>
2023-12-30 01:21:41 +0800

    12/30/2023 Update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1703783699",
+        "cacheTag": "1703870388",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "expo - overworld",
-            "last_rendertime": 1703781890,
+            "last_rendertime": 1703868746,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
